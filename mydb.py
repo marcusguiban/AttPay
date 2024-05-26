928db020f9e3e101ff9d84ee52d99ab8e9de37fd
Marcus Guiban <Guiban.marcus@gmail.com>
2024-05-26 21:41:21 +0800

    initial commit

---
+++ mydb.py
@@ -0,0 +1,23 @@
+# import pymongo
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
+# database = client["PayrollDatabase"]
+# print("Connected to MongoDB!")
+# employees_collection = database["PayrollCollection"]
+# print("Document inserted successfully!")
+# client.close()
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'password'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE AttPay")
+
+print("All Done!")
